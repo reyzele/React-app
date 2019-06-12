67cf922aafa6eeb331e296b84b70696caf11773b
Vadim Veksler <reyzele91@gmail.com>
2019-06-12 09:28:00 +0300

    add api

---
+++ public/precache-manifest.1330d6e1400ca70b5aef6a5d32c4b36a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "86174ca91831587fdb23",
+    "url": "/static/js/main.86174ca9.chunk.js"
+  },
+  {
+    "revision": "5d5de50561f87ecf0f5a",
+    "url": "/static/js/1.5d5de505.chunk.js"
+  },
+  {
+    "revision": "86174ca91831587fdb23",
+    "url": "/static/css/main.503bfbf5.chunk.css"
+  },
+  {
+    "revision": "5d5de50561f87ecf0f5a",
+    "url": "/static/css/1.a476c32c.chunk.css"
+  },
+  {
+    "revision": "1c5862bd07dacb6f3e84004be91db81a",
+    "url": "/index.html"
+  }
+];
